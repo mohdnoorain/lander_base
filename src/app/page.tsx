9defd08946d9502a20531f040c77d65ec4620854
Dheeru12503 <kumardheeraj12503@gmail.com>
2025-01-20 17:01:59 +0530

    Added Admin Login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import FeatureSection from "./section/FeatureSection/FeatureSection";
 import GallerySection from "./section/GallerySection/GallerySection";
 import NearBySection from "./section/NearBySection/NearBySection";
 import InfoSection from "./section/InfoSection/InfoSection";
+import Footer from "./components/Footer/Footer";
+
 
 const Gallery = async () => {
   try {
@@ -30,6 +32,7 @@ const DefaultPage: React.FC = () => {
       <GallerySection/>
       <NearBySection/>
       <InfoSection/>
+      <Footer/>
     </div>
   );
 };
