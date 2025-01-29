3dbe5c3d8dd3b87a4838998e016ee131b82d082b
Dheeru12503 <kumardheeraj12503@gmail.com>
2025-01-30 00:52:25 +0530

    Added Nearby Section and Payment Section

--- src/app/adminPanel/dashboard/cards/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import * as React from "react";
-
-export default function Cards(props: any) {
-  return <h2> page Cards </h2>;
-}
--- src/app/api/PaymentSectionData/route.ts
+++ src/app/api/PaymentSectionData/route.ts
@@ -1,17 +1,67 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-
-type ResponseData = {
-    message: string;
+import fs from "fs"
+import { DB_PATHS, RES_STATUS } from "../constants/commonConst";
+import RequestHelper from "../helpers/requestHelper";
+import { NextRequest, NextResponse } from "next/server";
+type paymentPlansItem = [string, string];
+type ApiResponse = {
+    paymentPlans: paymentPlansItem[];
+    LocationUrl: string;
 };
 
-export function GET(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
-    const PaymentSection = {
-        LocationUrl: "https://unitedestates.com/wp-content/uploads/2022/11/Binghatti-1.2-1024x576.jpg",
-        paymentPlans: [
-            "60% During Construction",
-            "40% On Handover",
-            "Q4 2023 TO Q1 2024",
-        ],
-    };
-    return Response.json(PaymentSection);
+export function GET(req: NextApiRequest, res: NextApiResponse<ApiResponse>) {
+    try {
+
+        const fileData = fs.readFileSync(DB_PATHS.PaymentSection, "utf-8");
+        const PaymentSectionData = JSON.parse(fileData);
+        return Response.json(PaymentSectionData
+
+        );
+
+    } catch (error: any) {
+
+        const data = {
+            message: error?.message || "Oops! something went wrong",
+            errorStack: "admin/feature"
+        };
+
+        return new Response(JSON.stringify(data),
+            {
+                status: RES_STATUS.badRequest,
+                headers: { "Content-Type": "application/json" }
+            }
+        );
+    }
 }
+
+export async function POST(request: NextRequest, response: NextResponse) {
+    try {
+        const body = await RequestHelper.parseBody(request);
+
+        const { } = body;
+        console.log("body", body);
+
+        fs.writeFileSync(DB_PATHS.PaymentSection, JSON.stringify(body, null, 2));
+
+        return new Response(JSON.stringify({}),
+            {
+                status: RES_STATUS.success,
+                headers: { "Content-Type": "application/json" }
+            }
+        );
+    } catch (error: any) {
+
+        const data = {
+            message: error?.message || "Oops! something went wrong",
+            errorStack: "admin/paymentsection"
+        };
+
+        return new Response(JSON.stringify(data),
+            {
+                status: RES_STATUS.badRequest,
+                headers: { "Content-Type": "application/json" }
+            }
+        );
+    }
+}
+
